d62e4d9790bdc19508bb16c72c0cf2b09ee34a19
Iwo <iwo.kaczmarzyk@gmail.com>
2023-08-20 19:19:22 +0200

    text-changed

--- src/components/InfoMsg.jsx
+++ src/components/InfoMsg.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function InfoMsg() {
   return (
     <div className='info-con'>
-        <h2>Hold and move the mouse or use the scroll wheel to zoom in or out and change the perspective</h2>
+        <h2>Hold and move the mouse or use the scroll wheel to zoom in or out</h2>
     </div>
   )
 }
